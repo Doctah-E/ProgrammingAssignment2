00754475f831506133cb00b63f425cdde9e29ea1
Doctah-E <e.j.van.ingen@vu.nl>
2023-08-11 14:42:35 +0200

    First attempt.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below create a way to store and retrieve inverted matrices
+## The first one assigns functions to an object using a list
+## The second one actually solves a matrix, unless it is already available 
 
-## Write a short comment describing this function
+
+# The function below creates an object with functions that can store and
+# retrieve matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    set_inv <- function(inv) i <<- inv
+    get_inv <- function() i
+    list(set = set, get = get,
+         set_inv = set_inv,
+         get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+# The function below checks whether the inverse of the matrix is already cached.
+# If so, it returns that object. If not the inverse is calculated and stored. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$get_inv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$set_inv(i)
+    i
 }
+
+
+
+
+## TESTING 
+
+m <- matrix(1:4, 2, 2)
+m_s <- solve(m)
+
+in_mat <- makeCacheMatrix(m)
+cacheSolve(in_mat)      # inverse is stored
+
+cacheSolve(in_mat)      # second time, matrix is retrieved from cache
+
+
+
+
+
+
